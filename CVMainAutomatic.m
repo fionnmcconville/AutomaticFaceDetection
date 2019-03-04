71bf796868e7e9f609ac91411e1e8834199abdc5
Zihong He <zhe03@qub.ac.uk>
2019-03-04 13:01:10 +0000

    1, show accuracy based on different number of k(for knn) in a figure table; 2, Adding Full images as an options for features descriptor.

--- CVMainAutomatic.m
+++ CVMainAutomatic.m
@@ -36,6 +36,9 @@ gabortrainFeatures = zeros(670, 19440);
 for i = 1:size(trainFeatures, 1)
     Im_train = reshape(trainFeatures(i,:),27,18);
     
+    %(Full image)This is to reshape full train features
+    %Full_train = reshape(trainFeatures(i,:),27,18);
+    
     Im_train = enhanceContrastHE(uint8(Im_train));
     %Hist Equalisation. Gives us a large improvement when used with gabor.
     %Accuracy is 0.9042
@@ -57,6 +60,9 @@ for i = 1:size(testFeatures, 1)
  
     Im_test = reshape(testFeatures(i,:),27,18);
     
+    %(Full image)This is to reshape full test features
+    %Full_test = reshape(testFeatures(i,:),27,18);
+    
     Im_test = enhanceContrastHE(uint8(Im_test));
     %Hist Equalisation. Gives us a large improvement when used with gabor.
     %Accuracy is 0.9042
@@ -70,22 +76,17 @@ for i = 1:size(testFeatures, 1)
 end
 
 %%Combine train features and test features for cross-validation model
-gaborFeatures = gabortrainFeatures + gabortestFeatures;
-%% Training models - Full images
-%Supervised Nearest Neighbour Training
-%modelNN = NNtraining(trainFeatures, trainLabs);
+Features = gabortrainFeatures + gabortestFeatures;
 
-%Supervised SVM Training
-%modelSVM = SVMtraining(trainFeatures, trainLabs);
-
+%(Full image)
+%Features = Full_train + Full_test;
 %% Training models - Gabor features
 %modelNN = NNtraining(gabortrainFeatures, trainLabs);
 
 
 %1, Cross-validation model with SVM
-%[testFeatures, testLabs] = loadFaceImages('face_test.cdataset', 1);
-%Features = trainFeatures;
-%modelSVM = SVMtraining(gaborFeatures, Labs);
+
+%modelSVM = SVMtraining(Features, Labs);
 
 %Using cross-validation to build the classification model
 %svmmodel = crossval(modelSVM);
@@ -95,14 +96,26 @@ gaborFeatures = gabortrainFeatures + gabortestFeatures;
 
 
 %2, Cross-validation model with k-nearest neighbor
-%[testFeatures, testLabs] = loadFaceImages('face_test.cdataset', 1);
-%Features = trainFeatures;
-modelKNN = CVknn(gaborFeatures, trainLabs);
+%Create a table to compare accuracy on different number of k
 
+accuracy = [];
+k_number_knn = [];
+
+for i = 1:10
+modelKNN = CVknn(Features, trainLabs, i);
 
 %Find the cross-validated loss of classifier
 loss = kfoldLoss(modelKNN);
 
 %Estimate accuracy of model
 Accuracy = 1 - loss;
-disp(Accuracy);
+
+k_number_knn = [k_number_knn;i];
+accuracy = [accuracy;Accuracy];
+end
+
+%Show table in a figure to estimate the highest accuracy
+knn_Accuracy_on_different_number_of_k = table(k_number_knn, accuracy);
+
+f = uifigure;
+uitable(f, 'Data', knn_Accuracy_on_different_number_of_k);
