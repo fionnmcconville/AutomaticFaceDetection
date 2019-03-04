74acf39b2acbc3ef6ddd7cd10920f114d37f0dfb
Zihong He <zhe03@qub.ac.uk>
2019-03-04 21:35:40 +0000

    1, Adding input variable: trainLabs and testLabs 2, Combine Labels and Features together by row 3, Delete "Select" and allocate Features and Labels directly from Features and Labs

--- Halfhalf.m
+++ Halfhalf.m
@@ -1,22 +1,28 @@
-function [trainFeatures, testFeatures] = Halfhalf(trainFeatures, testFeatures, trainLabs, testLabs)
+function [trainFeatures, testFeatures, trainLabs, testLabs] = Halfhalf(trainFeatures, testFeatures, trainLabs, testLabs)
 
 
 
-%Combine two set of Features together
-Features = trainFeatures + testFeatures;
+%Combine two set of Features and labels together
+Features = [trainFeatures ; testFeatures];
+Labs = [trainLabs; testLabs];
+
 
 %Number of total features
-n = numel(trainLabs + testLabs);
+n = size(trainLabs) + size(testLabs);
 
 %Divided Features into train and test set in average
 %Consider the situation that n is an odd number
-if mod(n,2)==0
-        trainFeatures = select(Features, 1:n/2);
-        testFeatures = select(Features, n/2+1:n);
-elseif mod(n,2)==1
-        trainFeatures = select(Features, 1:(n+1)/2);
-        testFeatures = select(Features, (n+1)/2+1:n);
-        
+if mod(n(1,1),2)==0
+        trainFeatures = Features(1:n/2,:);
+        testFeatures = Features(n/2+1:n,:);
+        trainLabs = Features(1:n/2,:);
+        testLabs = Features(n/2+1:n,:);
+elseif mod(n(1,1),2)==1
+        trainFeatures = Features(1:(n+1)/2,:);
+        testFeatures = Features((n+1)/2+1:n,:);
+        trainLabs = Features(1:n/2,:);
+        testFeatures = Features((n+1)/2+1:n,:);
+end
         
         
 
