e67bd131868148447f890597338ef4c62bbdcaaa
Zihong He <zhe03@qub.ac.uk>
2019-03-04 00:52:04 +0000

    1, Deciding features dividing way based on number of labels in total. 2, Set (gabor)trainFeatures and (gabor)testFeatures as input

--- Halfhalf.m
+++ Halfhalf.m
@@ -1,15 +1,25 @@
-function [trainFeatures, testFeatures] = Halfhalf('face_train.cdataset', 'face_test.cdataset')
+function [trainFeatures, testFeatures] = Halfhalf(trainFeatures, testFeatures, trainLabs, testLabs)
 
 
-%Load all Features from dataset
-[Features1, testLabs] = loadFaceImages('face_test.cdataset', 1);
-[Features2, testLabs] = loadFaceImages('face_train.cdataset', 1);
 
 %Combine two set of Features together
-Features = Features1 + Features2;
+Features = trainFeatures + testFeatures;
+
+%Number of total features
+n = numel(trainLabs + testLabs);
 
 %Divided Features into train and test set in average
-trainFeatures = select(Features, 1:30);
-testFeatures = select(Features, 31:59);
+%Consider the situation that n is an odd number
+if mod(n,2)==0
+        trainFeatures = select(Features, 1:n/2);
+        testFeatures = select(Features, n/2+1:n);
+elseif mod(n,2)==1
+        trainFeatures = select(Features, 1:(n+1)/2);
+        testFeatures = select(Features, (n+1)/2+1:n);
+        
+        
+        
+
+
 
 end
