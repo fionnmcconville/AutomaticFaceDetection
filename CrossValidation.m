de52f023ebc151a6bb2f52435024c01ea19db32b
Zihong He <zhe03@qub.ac.uk>
2019-03-23 00:51:46 +0000

    Renew Cross-Validation: 1, Perform n times classification and model building s to make sampling for test features different from each other in each time. 2, Calculate the average of accuracy and other elements. 3, Build the final model with all Features.

--- CrossValidation.m
+++ CrossValidation.m
@@ -1,9 +1,13 @@
-function [TrainFeatures, TestFeatures, trainLabs, testLabs] = CrossValidation(TrainFeatures, TestFeatures, trainLabs, testLabs, n)
+ function [TrainFeatures, TestFeatures, trainLabs, testLabs] = CrossValidation(TrainFeatures, TestFeatures, trainLabs, testLabs, n, fold_size, length, f)
 
-%Total label length of trainFeatures and testFeatures
-length = size(trainLabs) + size(testLabs);
+%%Cross-validation: For each classification: Divide Features into n folds, and select one of features
+%%in each fold as test features, perform classification fold times with
+%%each classification's test features location different from each other(1
+%%test features in the ending may repeat to suit the size of new features)
+%%.And then count the average accuracy of each classification's model.
 
-%Total Features of TrainFeatures and TestFeatures
+
+%Combine Features of TrainFeatures and TestFeatures
 Features = [TrainFeatures; TestFeatures];
 
 %Total labels of TrainFeatures and TestFeatures
@@ -12,45 +16,36 @@ Labs = [trainLabs;testLabs];
 %Reset TrainFeatures and TestFeatures to be 0
 %Reset TrainLabs and TestLabs to be 0
 %Later these will be used in build the new order of features and labels
+%Record n classifications
 TrainFeatures = [];
 TestFeatures = [];
 trainLabs = [];
 testLabs = [];
 
 
-%Number of features in each fold
-%Consider the situation that remainder exists
-if(mod(length(1,1),n) ~= 0)
-    fold = int16(length(1,1) / (n - 1) - mod(length(1,1) / (n - 1), 1)) + 1;
-else
-    fold = int16(length(1,1) / n);
-end
-
-
 %A pointer that record the number of Features have been filled in
 indice = 1;
-
 
 for i = 1:n
     
     %Break from the loop if all Features have be filled in
-    if indice > length
+    if indice == length(1,1)
         break
     end
-    %Decide test Feature location in each fold using random number
-    test_indice = randi([1 fold(1,1)],1);
+    %Decide test Feature location in each fold with input 'f'
+    test_indice = f;
     
     %Features in each fold
-    for j = (i - 1) * fold(1,1) + 1 : (i) * fold(1,1)
+    for j = (i - 1) * fold_size + 1 : i * fold_size 
         
         %Break from the loop if all Features have be filled in
-        if indice > length
+        if indice == length(1,1)
             break
         end
         
         %Check if it comes to test feature's location and decide whether to
         %place the current feature, trainFeatures, or testFeatures
-        if j ==(i - 1) * fold(1,1) + test_indice
+        if j == (i - 1) * fold_size + test_indice
             TestFeatures = [TestFeatures;Features(indice,:)];
             testLabs = [testLabs;Labs(indice,:)];
             indice  = indice + 1;
@@ -64,4 +59,5 @@ for i = 1:n
 end
 
 end
-        
+
+   
